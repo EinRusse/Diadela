bb3b529234cd8231004c44ce12d4f8b506edd4ba
EinRusse <pietro.ra2007@gmail.com>
2022-02-27 18:12:43 -0300

    Mais comandos basicos feitos e logica de CommandChecker refinada.

--- src/server/DiadelaMain/Commands/init.lua
+++ src/server/DiadelaMain/Commands/init.lua
@@ -1,16 +1,47 @@
 local module = {
 
     kill = {
-        function (player)
-            for _,p in pairs(player) do
+        function (params)
+            for _,p in pairs(params[1]) do
                 local play = game.Players:FindFirstChild(p)
                 if play ~= nil then
                     play.Character.Humanoid.Health = 0
                 end
             end
         end
+    },
+    re = {
+        function (params)
+            for _,p in pairs(params[1]) do
+                local play = game.Players:FindFirstChild(p)
+                if play ~= nil then
+                    play:LoadCharacter()
+                end
+            end
+        end
+    },
+    team = {
+        function (params)
+            for _,p in pairs(params[1]) do
+                local player = game.Players:FindFirstChild(p)
+                if player ~= nil then
+                    player.Team = params[2]
+                end
+            end
+        end
+    },
+    rteam = {
+        function (params)
+            for _,p in pairs(params[1])do
+                local teams = params[2]
+                local player = game.Players:FindFirstChild(p)
+                if player ~= nil then
+                    local i = math.random(1, #teams)
+                    player.Team = game.teams:FindFirstChild(teams[i])
+                end
+            end
+        end
     }
-    
 }
 
 return module
--- src/server/DiadelaMain/init.server.lua
+++ src/server/DiadelaMain/init.server.lua
@@ -6,12 +6,18 @@ local admins = require(script.Admin)
 
 local function CommandChecker(player, message)
     if message ~= nil then
-        local parameter = string.split(message, " ")
-        local parameters = string.split(parameter[2], ",")
-        local m,c = string.gsub(message, ";", "")
-        local command = string.split(m, " ")
-        if c == 1 then
-            if table.find(admins.Admins, player.Name) ~= nil then
+        if table.find(admins.Admins, player.Name) ~= nil then
+            message = string.split(message, " ")
+            local commando = message[1]
+            table.remove(message, 1)
+            local parameters = {}
+            for _,n in pairs(message) do 
+                local parameter = string.split(n, ",")
+                table.insert(parameters, parameter)
+            end
+            local m,c = string.gsub(commando, ";", "")
+            local command = string.split(m, " ")
+            if c == 1  then
                 local execute = commands[command[1]]
                 if execute ~= nil then
                     execute[1](parameters)
