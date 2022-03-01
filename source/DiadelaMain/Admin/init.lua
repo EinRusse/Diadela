861313694c0cfe5b6e3a0f6616c80f5247524f04
EinRusse <pietro.ra2007@gmail.com>
2022-02-28 22:27:18 -0300

    Admin limpado.

--- source/DiadelaMain/Admin/init.lua
+++ source/DiadelaMain/Admin/init.lua
@@ -3,11 +3,9 @@
 local module = {
     Admins = {
         --Here you will put the players that you want to give Admin.
-        "CarlinhosSpanish"
     },
     Mods = {
         --Here you will put the players that you want to give Mod.
-        "Player2"
     },
     BanList = {
         --Here is the list of banned players in your place.
